88816deae1d2d4ac278050132e91c1f26e841780
anzennadev <contact@anzenna.co>
2021-06-10 16:22:44 -0700

    :wrench: add basic content to page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import logo from './crop_center_image.png';
 import './App.css';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
+        <span className="font-link">
+          <img src={logo} className="App-logo" alt="logo" />
+          <br/> 
+          <br/> 
+          <Card text="dark" style={{ width: '72rem' }}>
+            <Card.Header>about</Card.Header>
+            <Card.Body>
+              <Card.Text>
+              安全な (anzen'na) is Japanese for <code>secure</code>. We are a smart contract security auditing firm based in United States. 
+              </Card.Text>
+              <Button variant="info" size="lg" href="mailto:contact@anzenna.co">Request an audit</Button>
+            </Card.Body>
+          </Card>
+        </span>
       </header>
+
     </div>
   );
 }
